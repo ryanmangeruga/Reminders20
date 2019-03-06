4bf5d25bd9ea6d700fa8e43778da3bf1cac6df32
ryanmangeruga <rmangeruga4@gmail.com>
2019-03-06 09:59:32 -0500

    03/06/2019
    Contains error in activity_main.xml involving changing the theme

--- app/src/main/java/com/example/ryan_mangeruga/reminders20/MainActivity.java
+++ app/src/main/java/com/example/ryan_mangeruga/reminders20/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.ryan_mangeruga.reminders20;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addButton = (Button) findViewById(R.id.AddButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+
     }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+
+    }
+
 }
