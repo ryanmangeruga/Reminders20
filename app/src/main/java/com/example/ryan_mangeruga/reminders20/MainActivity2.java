c74eaed13b72c51b9402f915a4c081c64ac1a1c5
ryanmangeruga <rmangeruga4@gmail.com>
2019-04-02 10:05:28 -0400

    04/02/2019 v2

--- app/src/main/java/com/example/ryan_mangeruga/reminders20/MainActivity2.java
+++ app/src/main/java/com/example/ryan_mangeruga/reminders20/MainActivity2.java
@@ -2,12 +2,67 @@ package com.example.ryan_mangeruga.reminders20;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    private TextView w1Header;
+
+    private TextView w1r1c1;
+    private TextView w1r1c2;
+    private TextView w1r1c3;
+
+    private TextView w1r2c1;
+    private TextView w1r2c2;
+    private TextView w1r2c3;
+
+    private TextView w1r3c1;
+    private TextView w1r3c2;
+    private TextView w1r3c3;
+
+    private TextView w1r4c1;
+    private TextView w1r4c2;
+    private TextView w1r4c3;
+
+    private TextView w1r5c1;
+    private TextView w1r5c2;
+    private TextView w1r5c3;
+
+    private TextView w1r6c1;
+    private TextView w1r6c2;
+    private TextView w1r6c3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        w1Header = (TextView) findViewById(R.id.week1Header);
+
+        w1r1c1 = (TextView) findViewById(R.id.week1r1c1);
+        w1r1c2 = (TextView) findViewById(R.id.week1r1c2);
+        w1r1c3 = (TextView) findViewById(R.id.week1r1c3);
+
+        w1r2c1 = (TextView) findViewById(R.id.week1r2c1);
+        w1r2c2 = (TextView) findViewById(R.id.week1r2c2);
+        w1r2c3 = (TextView) findViewById(R.id.week1r2c3);
+
+        w1r3c1 = (TextView) findViewById(R.id.week1r3c1);
+        w1r3c2 = (TextView) findViewById(R.id.week1r3c2);
+        w1r3c3 = (TextView) findViewById(R.id.week1r3c3);
+
+        w1r4c1 = (TextView) findViewById(R.id.week1r4c1);
+        w1r4c2 = (TextView) findViewById(R.id.week1r4c2);
+        w1r4c3 = (TextView) findViewById(R.id.week1r4c3);
+
+        w1r5c1 = (TextView) findViewById(R.id.week1r5c1);
+        w1r5c2 = (TextView) findViewById(R.id.week1r5c1);
+        w1r5c3 = (TextView) findViewById(R.id.week1r5c3);
+
+        w1r6c1 = (TextView) findViewById(R.id.week1r6c1);
+        w1r6c2 = (TextView) findViewById(R.id.week1r6c2);
+        w1r6c3 = (TextView) findViewById(R.id.week1r6c3);
+
     }
 }
